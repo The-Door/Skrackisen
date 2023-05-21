1efd77aa1c69778b203cd6b820e61adc090bd304
Moloolo <milosnoopdog@gmail.com>
2023-05-21 21:25:13 +0200

    Movement
    First person

--- Spelet/Assets/PlayerMovement.cs
+++ Spelet/Assets/PlayerMovement.cs
@@ -1,22 +1,67 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public float moveSpeed = 5f;  // The speed at which the player moves
-    public float lookSpeed = 2f;  // The speed at which the player rotates the camera
 
+public class Jump : MonoBehaviour
+{
+    public float jumpForce = 5f;  // The force applied when the player jumps
+    private bool isJumping = false;
     private Rigidbody rb;
-    private Camera playerCamera;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Check if the spacebar is pressed and the player is on the ground
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            JumpAction();
+        }
+    }
+
+    private void JumpAction()
+    {
+        // Apply the jump force to the player
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        isJumping = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player has landed on the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+}
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;  // The speed at which the player moves
+    public float lookSpeed = 2f;  // The speed at which the player rotates the camera
+    public float jumpForce = 5f;  // The force applied when the player jumps
+    public float groundCheckDistance = 0.2f;  // The distance to check if the player is grounded
+    public LayerMask groundMask;  // The layer mask for the ground objects
+
+    private CharacterController controller;
+    private Camera playerCamera;
+    private bool isGrounded;
+    private Vector3 playerVelocity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
         playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
     }
 
     private void Update()
     {
+        // Check if the player is grounded
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
         // Get input from the player
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
@@ -24,10 +69,26 @@ public class PlayerMovement : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y");
 
         // Calculate the movement vector
-        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement) * moveSpeed;
+        Vector3 movement = transform.right * horizontalMovement + transform.forward * verticalMovement;
+        movement *= moveSpeed;
 
-        // Apply the movement to the player's position
-        transform.Translate(movement * Time.deltaTime);
+        // Apply gravity
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        // Apply jumping
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y);
+        }
+
+        // Apply movement to the character controller
+        controller.Move((movement + playerVelocity) * Time.deltaTime);
+
+        // Apply gravity
+        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
 
         // Rotate the player horizontally
         transform.Rotate(Vector3.up, mouseX * lookSpeed);
